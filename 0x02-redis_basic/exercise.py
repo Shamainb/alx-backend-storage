96e93e52e577bda8dc899210d3715fd1b9331703
Shamainb <shamainbhengesa60@gmail.com>
2024-05-15 23:10:41 +0200

    strings to Redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable
 
 
 class Cache:
@@ -15,3 +15,19 @@ class Cache:
             data = str(data)
         self._redis.set(key, data)
         return key
+
+
+def get(self, key: str, fn: Callable[[bytes], Union[str, bytes, int, float]] =
+        Noe) -> Union[str, bytes, int, float, None]:
+    data = self._redis.get(key)
+    if data is None:
+        return None
+    if fn is not None:
+        data = fn(data)
+        return data
+
+    def get_str(self, key: str) -> str:
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> int:
+        return self.get(key, fn=int)
